3165fda7daf2bc3fd2283e80b68d1c9cfb1136e6
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-29 16:32:03 -0400

    Added questions array

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 // Consider if/else or choices for user input to render appropriate elements.
 // Render logo elements in a new document titled 'logo.svg'
 // Usin
+
+const questions = [
+    {
+        type:"checkbox",
+        message:"Select a shape",
+        choices: ["Square", "Circle", "Triangle"],
+        name: "shape"
+    },
+    {
+        type:"input",
+        message: "Please input a color",
+        name: "color"
+    },
+    {
+        type: "input",
+        message: "Please type up to three letters",
+        name: "text"
+    }
+]
+
+inquirer.prompt(questions)
+.then
