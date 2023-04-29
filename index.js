a20d09b1721370606061ac22e56020d2bc122200
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-29 14:32:26 -0400

    Created initial files and directories.

---
+++ index.js
@@ -0,0 +1,5 @@
+// Use inquirer to create a command-line application
+// Take the inputs from inquirer and use them to render the various elements for logo creation.
+// Consider if/else or choices for user input to render appropriate elements.
+// Render logo elements in a new document titled 'logo.svg'
+// Usin
