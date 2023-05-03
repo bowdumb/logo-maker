3b10551e68bf2ec32b5bf78dc947dda548af594b
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-03 18:12:39 -0400

    Got the initial inquirer question prompts to run without error.

--- index.js
+++ index.js
@@ -3,25 +3,54 @@
 // Consider if/else or choices for user input to render appropriate elements.
 // Render logo elements in a new document titled 'logo.svg'
 // Usin
+const inquirer = require('inquirer');
+const fs = require('fs');
+const logo = require('./test.svg');
 
 const questions = [
     {
         type:"checkbox",
-        message:"Select a shape",
+        message:"Select a shape for your logo.",
         choices: ["Square", "Circle", "Triangle"],
         name: "shape"
     },
     {
         type:"input",
-        message: "Please input a color",
+        message: "Please type a color for the shape of your logo.",
         name: "color"
     },
     {
         type: "input",
-        message: "Please type up to three letters",
+        message: "Please type up to three letters for your logo.",
         name: "text"
+    },
+    {
+
+        type: "input",
+        message: "Please type a color for your logo.",
+        name: "textColor"
+
     }
 ]
 
-inquirer.prompt(questions)
-.then
+questionPrompt = () => {
+    inquirer.prompt(questions)
+    .then((questionData) => {
+        if (questionData.text.length > 3) {
+            console.log('Please enter no more than three characters for your text input!');
+            return;
+        } else {
+            console.log('We got answers!');
+            
+            writeToFile("logo.svg", questionData);
+        }
+        
+    })
+};
+
+writeToFile = (filename, data) => {
+
+};
+// }
+
+questionPrompt();
