0340bf58b2a1ca925846a193b84a1a552527af82
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-04 18:46:15 -0400

    Got a rough working version.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 // Usin
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapeVerdict = require('./lib/shapes');
-// const {Square, Circle, Triangle} = require('./lib/shapes');
+let svgShape = require('./lib/shapes');
+const {Square, Circle, Triangle} = require('./lib/shapes');
+console.log({Square, Circle, Triangle})
 
 const questions = [
     {
@@ -40,13 +41,13 @@ const
 questionPrompt = () => {
     inquirer.prompt(questions)
     .then((answers) => {
-        const questionAnswers = shapeVerdict ({
+        const questionAnswers =  {
             shape: `${answers.shape}`,
             colorShape: `${answers.colorShape}`,
             text: `${answers.text}`,
             textColor: `${answers.textColor}`
 
-        })
+        }
         
 
         if (answers.text.length > 3) {
@@ -58,14 +59,64 @@ questionPrompt = () => {
            
             console.log(answers);
         }
-        
+    writeToFile('logo.svg', questionAnswers);
     })
 
 };
 // Generate a new SVG using the user inputs from the inquirer prompts.
-// generateSvg = (answers) {
-//     const svg = 
+function writeToFile(fileName, questionAnswers) {
+    let svgShape;
+    
+    if (questionAnswers.shape === "Circle") {
+        svgShape = `<circle cx="50" cy="50" r="40" fill="${questionAnswers.colorShape}"/>`
+    
+    }else if (questionAnswers.shape === "Triangle") {
+        svgShape = `<polygon points="150, 18 244, 182 56, 182" fill="${questionAnswers.colorShape}"/>`
+    } else {
+        svgShape = `<rect x="10" y="10" width="80" height="80" fill="${questionAnswers.colorShape}"/>`
+    };
 
+    const { shape, colorShape, text, textColor } = questionAnswers;
+    const svgValues = `
+        <svg width="300" height="300">
+            <${svgShape} fill="${colorShape}">
+                <text x="50" y="50" font-size="20" fill="${textColor}">${text}</text>
+            </${shape}>
+        </svg>`
+
+      
+    ;
+    fs.writeFile(fileName, svgValues, (error) => {
+        if(error) {
+            console.log('Error writing SVG to file.');
+        } else {
+            console.log('Success!');
+        }
+    })
+}
+
+
+
+
+
+    //     fs.writeFile('logo.svg', JSON.stringify(questionAnswers), (error) => {
+        
+//         let shapeType;
+//         if(questionAnswers.shape === "Circle") {
+//             shapeType = new Circle();
+//             return `<circle cx="150" cy="115" r="80" fill="${questionAnswers.colorShape}"/>`
+//         }
+        
+        
+        
+        
+//         if(error) {
+//             console.log('Error writing SVG to file.');
+//         } else {
+//             console.log('Success!');
+//         }
+//     })
 // }
+
 
 questionPrompt();
